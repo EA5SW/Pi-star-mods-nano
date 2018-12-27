cccc12328babb11b5a87d4fb6d66fe4e275f52d6
EA5SW <32942778+EA5SW@users.noreply.github.com>
2018-12-27 17:53:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ echo "Pi Star OLED - Blas de Lezo v1.0"
 echo "Parando Pi-Star original"
 sudo sudo systemctl stop mmdvmhost.service
 echo "Renombrando PiStar"
+rpi-rw
 sudo mv /usr/local/bin/MMDVMHost /usr/local/bin/MMDVMHost.pistar
 echo "Instalando EA5SW - OLED Blas de Lezo"
 sudo cp ./MMDVMHost.EA5SW /usr/local/bin/MMDVMHost
